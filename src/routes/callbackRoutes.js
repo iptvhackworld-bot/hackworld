ce191983ca5a70cf7808a77a5b1db822e7d53439
HackWorld <iptvhackworld@gmail.com>
2026-05-18 11:00:21 +0200

    Fix callback export

--- src/routes/callbackRoutes.js
+++ src/routes/callbackRoutes.js
@@ -1,59 +1,432 @@
-/*
+module.exports = (bot) => {
 
-bot.action(
+  /*
+  |--------------------------------------------------------------------------
+  | IMPORTS
+  |--------------------------------------------------------------------------
+  */
 
-  'admin_analytics',
+  const {
 
-  analyticsHandler.showAnalytics
+    showMainMenu
 
-)
+  } = require(
+    '../handlers/startHandler'
+  )
 
-bot.action(
+  const {
 
-  'admin_backup',
+    categoryHandler
 
-  backupHandler.createBackup
+  } = require(
+    '../handlers/categoryHandler'
+  )
 
-)
+  const navigationHandler =
+  require(
+    '../handlers/navigationHandler'
+  )
 
-bot.action(
+  const {
 
-  'admin_permissions',
+    openStaffMenu,
 
-  permissionHandler.openPermissions
+    showAdmins,
 
-)
+    showModerators,
 
-bot.action(
+    showTopUsers
 
-  'admin_system',
+  } = require(
+    '../handlers/staffHandler'
+  )
 
-  systemHandler.openSystemPanel
+  const {
 
-)
+    rateBot
 
-bot.action(
+  } = require(
+    '../handlers/ratingHandler'
+  )
 
-  'admin_security',
+  const {
 
-  securityHandler.openSecurityPanel
+    openAdminPanel
 
-)
+  } = require(
+    '../handlers/adminHandler'
+  )
 
-bot.action(
+  const {
 
-  'admin_categories',
+    openAddContent,
 
-  categoryManagerHandler.openCategoryManager
+    handleAddContent
 
-)
+  } = require(
+    '../handlers/adminContentHandler'
+  )
 
-bot.action(
+  const {
 
-  'admin_staff_manager',
+    showContentList
 
-  staffManagerHandler.openStaffManager
+  } = require(
+    '../handlers/adminEditHandler'
+  )
 
-)
+  const {
 
-*/
+    startBroadcast,
+
+    handleBroadcastText,
+
+    handleBroadcastMedia
+
+  } = require(
+    '../handlers/broadcastHandler'
+  )
+
+  const {
+
+    openUsersPanel,
+
+    openUserPanel,
+
+    banUser,
+
+    unbanUser,
+
+    resetMoney,
+
+    resetXP
+
+  } = require(
+    '../handlers/manageUsersHandler'
+  )
+
+  /*
+  |--------------------------------------------------------------------------
+  | USER PANEL
+  |--------------------------------------------------------------------------
+  */
+
+  bot.action(
+
+    /user_(\d+)/,
+
+    openUserPanel
+
+  )
+
+  bot.action(
+
+    'ban_user',
+
+    banUser
+
+  )
+
+  bot.action(
+
+    'unban_user',
+
+    unbanUser
+
+  )
+
+  bot.action(
+
+    'reset_money',
+
+    resetMoney
+
+  )
+
+  bot.action(
+
+    'reset_xp',
+
+    resetXP
+
+  )
+
+  /*
+  |--------------------------------------------------------------------------
+  | RATING
+  |--------------------------------------------------------------------------
+  */
+
+  bot.action(
+
+    'rate_1',
+
+    (ctx) => rateBot(
+      ctx,
+      1
+    )
+
+  )
+
+  bot.action(
+
+    'rate_5',
+
+    (ctx) => rateBot(
+      ctx,
+      5
+    )
+
+  )
+
+  /*
+  |--------------------------------------------------------------------------
+  | ADMIN PANEL
+  |--------------------------------------------------------------------------
+  */
+
+  bot.action(
+
+    'admin_panel',
+
+    openAdminPanel
+
+  )
+
+  /*
+  |--------------------------------------------------------------------------
+  | ADMIN ADD CONTENT
+  |--------------------------------------------------------------------------
+  */
+
+  bot.action(
+
+    'admin_add_content',
+
+    openAddContent
+
+  )
+
+  /*
+  |--------------------------------------------------------------------------
+  | ADMIN EDIT CONTENT
+  |--------------------------------------------------------------------------
+  */
+
+  bot.action(
+
+    'admin_edit_content',
+
+    showContentList
+
+  )
+
+  /*
+  |--------------------------------------------------------------------------
+  | ADMIN BROADCAST
+  |--------------------------------------------------------------------------
+  */
+
+  bot.action(
+
+    'admin_broadcast',
+
+    startBroadcast
+
+  )
+
+  /*
+  |--------------------------------------------------------------------------
+  | ADMIN USERS
+  |--------------------------------------------------------------------------
+  */
+
+  bot.action(
+
+    'admin_users',
+
+    openUsersPanel
+
+  )
+
+  /*
+  |--------------------------------------------------------------------------
+  | HANDLE TEXT
+  |--------------------------------------------------------------------------
+  */
+
+  bot.on(
+
+    'text',
+
+    handleAddContent
+
+  )
+
+  bot.on(
+
+    'text',
+
+    handleBroadcastText
+
+  )
+
+  /*
+  |--------------------------------------------------------------------------
+  | HANDLE MEDIA
+  |--------------------------------------------------------------------------
+  */
+
+  bot.on(
+
+    'photo',
+
+    handleBroadcastMedia
+
+  )
+
+  bot.on(
+
+    'video',
+
+    handleBroadcastMedia
+
+  )
+
+  /*
+  |--------------------------------------------------------------------------
+  | RULES
+  |--------------------------------------------------------------------------
+  */
+
+  bot.action(
+
+    'accept_rules',
+
+    async (ctx) => {
+
+      await ctx.deleteMessage()
+
+      await showMainMenu(ctx)
+
+    }
+
+  )
+
+  bot.action(
+
+    'decline_rules',
+
+    async (ctx) => {
+
+      await ctx.reply(
+        '❌ Vous devez accepter le règlement.'
+      )
+
+    }
+
+  )
+
+  /*
+  |--------------------------------------------------------------------------
+  | STAFF
+  |--------------------------------------------------------------------------
+  */
+
+  bot.action(
+
+    'cat_staff',
+
+    openStaffMenu
+
+  )
+
+  bot.action(
+
+    'show_admins',
+
+    showAdmins
+
+  )
+
+  bot.action(
+
+    'show_mods',
+
+    showModerators
+
+  )
+
+  bot.action(
+
+    'show_top_users',
+
+    showTopUsers
+
+  )
+
+  /*
+  |--------------------------------------------------------------------------
+  | CATEGORIES
+  |--------------------------------------------------------------------------
+  */
+
+  bot.action(
+
+    /cat_(.+)/,
+
+    categoryHandler
+
+  )
+
+  /*
+  |--------------------------------------------------------------------------
+  | NAVIGATION
+  |--------------------------------------------------------------------------
+  */
+
+  bot.action(
+
+    /next_(.+)_(\d+)/,
+
+    async (ctx) => {
+
+      await navigationHandler(
+        ctx,
+        'next'
+      )
+
+    }
+
+  )
+
+  bot.action(
+
+    /prev_(.+)_(\d+)/,
+
+    async (ctx) => {
+
+      await navigationHandler(
+        ctx,
+        'prev'
+      )
+
+    }
+
+  )
+
+  /*
+  |--------------------------------------------------------------------------
+  | BACK MENU
+  |--------------------------------------------------------------------------
+  */
+
+  bot.action(
+
+    'back_menu',
+
+    showMainMenu
+
+  )
+
+}
