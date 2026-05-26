73badb447062b5dd479c807c204de0ef2dfe842b
HackWorld <iptvhackworld@gmail.com>
2026-05-26 19:11:17 +0200

    isolate callback routes

--- src/core/routes.js
+++ src/core/routes.js
@@ -2,28 +2,12 @@ const registerRoutes =
 (bot) => {
 
   console.log(
-    'LOAD commandRoutes'
-  )
-
-  // require(
-  //   '../routes/commandRoutes'
-  // )(bot)
-
-  console.log(
     'LOAD callbackRoutes'
   )
 
   require(
     '../routes/callbackRoutes'
   )(bot)
-
-  console.log(
-    'LOAD staffCommandRoutes'
-  )
-
-  // require(
-  //   '../routes/staffCommandRoutes'
-  // )(bot)
 
   console.log(
     '✅ Routes chargées'
